f380e46e1d2f884a8f1412613da49fd5824470f0
jrcaranddang <jrcaranddang@gmail.com>
2017-02-10 11:11:48 -0800

    added hero section, logo, top-right

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { DynamicGridComponent } from './dynamic-grid/dynamic-grid.component';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { MaterialModule } from '@angular/material';
+import 'hammerjs';
+
+import { AppComponent } from './app.component';
+import { DynamicGridComponent } from './dynamic-grid/dynamic-grid.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DynamicGridComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    MaterialModule.forRoot()
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
