75a5012450f8e100710b99ab14dcb79a91398dfb
jrcaranddang <jrcaranddang@gmail.com>
2017-02-10 22:01:35 -0800

    added project grid, contact form, hero

--- src/app/dynamic-grid/dynamic-grid.component.ts
+++ src/app/dynamic-grid/dynamic-grid.component.ts
@@ -7,12 +7,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DynamicGridComponent implements OnInit {
   tiles = [
-    {text: 'One', cols: 3, rows: 1, color: 'lightblue'},
-    {text: 'Two', cols: 1, rows: 2, color: 'lightgreen'},
-    {text: 'Three', cols: 1, rows: 1, color: 'lightpink'},
-    {text: 'Four', cols: 2, rows: 1, color: '#DDBDF1'},
-    {text: 'Five', cols: 2, rows: 1, color: '#DDBDF1'},
-    {text: 'Six', cols: 2, rows: 1, color: '#DDBDF1'},
+    {text: 'gutter', cols: 1, rows: 29, color: 'none'},
+    {text: 'project one', cols: 6, rows: 6, color: 'lightpink'},
+    {text: 'description', cols: 4, rows: 6, color: 'lightblue'},
+    {text: 'gutter', cols: 1, rows: 29, color: 'none'},
+    {text: 'divider', cols: 10, rows: 1, color: 'none'},
+    {text: 'description', cols: 4, rows: 6, color: 'lightblue'},
+    {text: 'project two', cols: 6, rows: 6, color: 'lightpink'},
+    {text: 'divider', cols: 10, rows: 1, color: 'none'},
+    {text: 'divider', cols: 2, rows: 5, color: 'none'},
+    {text: 'project three', cols: 4, rows: 5, color: 'lightblue'},
+    {text: 'divider', cols: 4, rows: 3, color: 'none'},
+    {text: 'description', cols: 4, rows: 3, color: 'lightpink'},
+    {text: 'divider', cols: 9, rows: 1, color: '#555'},
+    {text: 'divider', cols: 1, rows: 3, color: '#444'},
+    {text: 'divider', cols: 4, rows: 4, color: '#777'},
+    {text: 'project four', cols: 4, rows: 5, color: 'lightblue'},
+    {text: 'divider', cols: 1, rows: 3, color: '#aaa'},
+    {text: 'description', cols: 4, rows: 3, color: 'lightpink'},
+    {text: 'divider', cols: 10, rows: 1, color: 'none'},
   ];
 
   constructor() { }
